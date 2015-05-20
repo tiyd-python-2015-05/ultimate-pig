2ea1070f514de02b50c4e921aa04146f5b5a8a96
sovello <sovellohpmgani@gmail.com>
2015-05-20 19:34:34 -0400

    first part of the game is working. the Optimal Solitaire Player is winning in every race! :)

--- ultimate_pig.py
+++ ultimate_pig.py
@@ -1,7 +1,7 @@
 import random
 import sys
 
-class Player(object):
+class Player:
 
     def __init__(self, total_score = 0, name="BasePlayer"):
         self.total_score = total_score
@@ -34,7 +34,7 @@ class Player(object):
         return "Your total score was: {} point(s)".format(self.total_score)
 
 
-    def play(self, rounds):
+    def play(self, rounds, turns = 0):
         current_turn = 0
         while current_turn < rounds:
             if self.roll != 1:
@@ -42,10 +42,42 @@ class Player(object):
                 current_turn += 1
             else:
                 current_turn += 1
-            print("This is turn {} with score {}".format(current_turn, self.total_score))
+            #print("This is turn {} with score {}".format(current_turn, self.total_score))
+        return round(self.total_score/rounds,2)
 
 
-class Game(object):
+class OptimalPlayer(Player):
+    current_turn = 1
+    def play(self, rounds, turns = 7):
+        while self.current_turn < rounds:
+            #print("Playing round {}".format(self.current_turn))
+            for i in range(1,4):
+                if self.roll != 1:
+                    self.update_score()
+                    #print("Roll {}: Win".format(i))
+                else:
+                    #print("Roll {}: Lose".format(i))
+                    break
+            self.current_turn += 1
+        return round(self.total_score/rounds,2)
+
+
+class OptimalSolitairePlayer(Player):
+    current_turn = 1
+    def play(self, rounds, turns = 7):
+        while self.current_turn < rounds:
+            #print("Playing round {}".format(self.current_turn))
+            for i in range(1,6):
+                if self.roll != 1:
+                    self.update_score()
+                    #print("Roll {}: Win".format(i))
+                else:
+                    #print("Roll {}: Lose".format(i))
+                    break
+            self.current_turn += 1
+        return round(self.total_score/rounds,2)
+
+class Game():
 
     def __init__(self, current_player, rounds = 0, winning_score = 0 ):
         self.rounds = rounds
@@ -61,13 +93,16 @@ class Game(object):
 
 
     def game(self):
-        while this_round < self.rounds:
-            player.play()
-
+        if winning_score == 0:
+            while this_round < self.rounds:
+                player.play()
+        else:
+            pass
 
 
 if __name__ == '__main__':
-    player = Player()
+    player = OptimalSolitairePlayer()
+    oplayer = OptimalPlayer()
     game = Game(current_player = player, rounds = 7)
     player.play(game.rounds)
     print(player.print_score())
